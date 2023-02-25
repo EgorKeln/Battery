4b0122af600287d7558bde7711a3889d23abfad2
Keln <122953267+EgorKeln@users.noreply.github.com>
2023-02-25 14:44:31 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ function initBattery() {
         updateBattery = () => {
             let level = Math.floor(batt.level * 100);
             Bpercentage.innerHTML = level + "%";
-            batteryLiquid.getElementsByClassName.height = `${parseInt(batt.level * 100)}%`;
+            batteryLiquid.style.height = `${parseInt(batt.level * 100)}%`;
             if (level == 100) {
                 batteryStatus.innerHTML = `Battery Full <i class="ri-battery-2-fill green-color"></i>`;
                 batteryLiquid.style.height = "103%";
-            } else if (level <= 20 & !batt.charging) {
+            } else if (level <= 20 &! batt.charging) {
                 batteryStatus.innerHTML = `Low Charge <i class="ri-plug-line animated-red animated-red"></i>`;
             } else if (batt.charging){
                 batteryStatus.innerHTML = `Charging ... <i class="ri-flashlight-line animated-green"></i>`;
